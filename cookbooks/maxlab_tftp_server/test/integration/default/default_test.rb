23ac121507258359a98a23a36278bb20c0b3e161
Maxwell Spangler <maxwell@maxwellspangler.com>
2020-02-08 22:54:08 -0700

    Minor change to *comment* of maxlab_tftp_server cookbook's default_test.rb file.  Fixing the cookbook's name from dashes to underscores. Minor housekeeping.

--- cookbooks/maxlab_tftp_server/test/integration/default/default_test.rb
+++ cookbooks/maxlab_tftp_server/test/integration/default/default_test.rb
@@ -1,10 +1,9 @@
 # # encoding: utf-8
 
-# Inspec test for recipe maxlab-tftp-server::default
+# Inspec test for recipe maxlab_Stftp_server::default
 
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
-
 
 describe package('tftp') do
   it { should be_installed }
