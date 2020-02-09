fdf90649f605681e03732d6a5f37f52c20854deb
Maxwell Spangler <maxwell@maxwellspangler.com>
2020-02-08 22:53:15 -0700

    Fix comment in maxlab_nginx_repo default.rb test file to have its name and not the maxlab-tftp-server name it was copied from.

--- cookbooks/maxlab_nginx_repo/test/integration/deploy/default.rb
+++ cookbooks/maxlab_nginx_repo/test/integration/deploy/default.rb
@@ -1,6 +1,6 @@
 # # encoding: utf-8
 
-# Inspec test for recipe maxlab-tftp-server::default
+# Inspec test for recipe maxlab_nginx_repo::default
 
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
