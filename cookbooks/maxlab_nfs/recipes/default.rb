1dbd16b7440bfd9117160a0a8f3568c251d086ad
Maxwell Spangler <maxwell@maxwellspangler.com>
2020-02-10 22:10:09 -0700

    maxlab_nfs cookbook: Fixing two comments, no code.

---
+++ cookbooks/maxlab_nfs/recipes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook:: maxlab_nfs
+# Recipe:: default
+#
+# Copyright:: 2020, The Authors, All Rights Reserved.
+
+=begin
+#<
+This recipe performs no actions.
+#>
+=end
--- cookbooks/maxlab_nfs/test/integration/server/server.rb
+++ cookbooks/maxlab_nfs/test/integration/server/server.rb
@@ -1,6 +1,6 @@
 # # encoding: utf-8
 
-# Inspec test for recipe maxlab_nfs::deploy
+# Inspec test for recipe maxlab_nfs::server
 
 #
 # Test real node configurations to ensure that nfs is deployed
