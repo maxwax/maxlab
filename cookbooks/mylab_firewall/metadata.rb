9fba04fdebb090552e5012ca838a10bc52fc59d8
Maxwell Spangler <maxwell@maxwellspangler.com>
2020-02-10 22:16:42 -0700

    mylab_nfs: Just updating metadata.rb owner info.

--- cookbooks/mylab_firewall/metadata.rb
+++ cookbooks/mylab_firewall/metadata.rb
@@ -1,9 +1,9 @@
 name 'mylab_firewall'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
+maintainer 'Maxwell Spangler'
+maintainer_email 'maxcode@maxwellspangler.com'
 license 'All Rights Reserved'
-description 'Installs/Configures mylab_firewall'
-version '1.0.0'
+description 'Experimental wrapper cookbook to maxlab_firewall'
+version '1.0.1'
 chef_version '>= 14.0'
 
 # The `issues_url` points to the location where issues for this cookbook are
