3aa4d430f02459e686e67f5d5a06db2711414ce4
Maxwell Spangler <maxwell@maxwellspangler.com>
2020-02-11 00:20:50 -0700

    Changing initial version from 0.1.0 to 1.0.0.

--- cookbooks/mylab_tftp_server/metadata.rb
+++ cookbooks/mylab_tftp_server/metadata.rb
@@ -3,7 +3,7 @@ maintainer 'Maxwell Spangler'
 maintainer_email 'maxcode@maxwellspangler.com'
 license 'All Rights Reserved'
 description 'Experimental wrapper cookbook to maxlab_tftp_server'
-version '0.1.0'
+version '1.0.0'
 chef_version '>= 14.0'
 
 # The `issues_url` points to the location where issues for this cookbook are
