731b003f9f6e561b00799354df39c77b945542ec
Maxwell Spangler <maxwell@maxwellspangler.com>
2020-02-13 13:27:56 -0700

    Construct policyfiles to replace runlists & roles for maxlab 'production' nodes.

---
+++ policyfiles/maxlab_node_netinfra_primary.rb
@@ -0,0 +1,53 @@
+# Policyfile.rb - Describe how you want Chef Infra server to build your system.
+
+name 'maxlab_node_netinfra_primary'
+
+default_source :supermarket
+
+run_list "recipe[maxlab_base::deploy]",
+         "recipe[maxlab_firewall::base_firewalld]",
+         "recipe[maxlab_users::general]",
+         "recipe[maxlab_bind::deploy]",
+         "recipe[maxlab_dhcp::deploy]",
+         "recipe[maxlab_chrony::deploy]",
+         "recipe[maxlab_postfix::deploy]",
+         "recipe[maxlab_firewall::update_firewalld]"
+
+cookbook 'maxlab_base',     path: '../cookbooks/maxlab_base'
+cookbook 'maxlab_users',    path: '../cookbooks/maxlab_users'
+cookbook 'maxlab_firewall', path: '../cookbooks/maxlab_firewall'
+cookbook 'maxlab_bind',     path: '../cookbooks/maxlab_bind'
+cookbook 'maxlab_dhcp',     path: '../cookbooks/maxlab_dhcp'
+cookbook 'maxlab_chrony',   path: '../cookbooks/maxlab_chrony'
+cookbook 'maxlab_postfix',  path: '../cookbooks/maxlab_postfix'
+
+# From role base_firewall_maxlab_nas
+default['config_firewall']['base_config'] = 'base_firewall.maxlab.dmz'
+
+# From role client_chrony_maxlab
+default['instance_config_chrony']['instance'] = 'chrony.maxlab'
+default['instance_config_chrony']['instance_type'] = 'server'
+
+# From role service_binddns_instance_maxlab
+default['config_bind']['instance'] = 'dns_server.maxlab'
+
+# From role service_dhcp_instance_core
+#default['config_dhcp']['instance'] = 'dhcp_server.core.maxlab'
+default['dhcp']['instance_config_dhcp']['primary'] = true
+
+# From role service_postfix_instance_maxlab_postfix
+default['instance_config_postfix']['instance'] = 'maxlab_postfix'
+
+# Previously environment variables
+default['env']['maxlab']['repo_url']          = "http://repo.maxlab"
+default['env']['maxlab']['dist_url']          = "http://repo.maxlab/dist"
+default['env']['maxlab']['iso_url']           = "http://repo.maxlab/iso"
+default['env']['maxlab']['files_url']         = "http://repo.maxlab/files"
+default['env']['maxlab']['typefaces_url']     = "http://repo.maxlab/files/typefaces"
+default['env']['maxlab']['chef_url']          = "https://chef.maxlab/organizations/maxlab"
+default['env']['maxlab']['tftp_url']          = "http://repo.maxlab"
+default['env']['maxlab']['upstream_ntp']      = "2.centos.pool.ntp.org"
+default['env']['maxlab']['metric_submit_vip'] = "http://metric_submit.maxlab"
+default['env']['maxlab']['metric_query_vip']  = "http://metric_query.maxlab"
+default['env']['maxlab']['graphite_vip']      = "http://graphite.maxlab"
+default['env']['maxlab']['graphite_vip']      = "http://grafana.maxlab"
---
+++ policyfiles/maxlab_node_repo_primary.rb
@@ -0,0 +1,57 @@
+# Policyfile.rb - Describe how you want Chef Infra server to build your system.
+
+name 'maxlab_node_repo_primary'
+
+default_source :supermarket
+
+run_list "recipe[maxlab_base::deploy]",
+         "recipe[maxlab_firewall::base_firewalld]",
+         "recipe[maxlab_users::general]",
+         "recipe[maxlab_nginx_repo::deploy]",
+         "recipe[maxlab_tftp::deploy]",
+         "recipe[maxlab_pxe::deploy]",
+         "recipe[maxlab_chrony::deploy]",
+         "recipe[maxlab_postfix::deploy]",
+         "recipe[maxlab_firewall::update_firewalld]"
+
+cookbook 'maxlab_base',       path: '../cookbooks/maxlab_base'
+cookbook 'maxlab_users',      path: '../cookbooks/maxlab_users'
+cookbook 'maxlab_firewall',   path: '../cookbooks/maxlab_firewall'
+cookbook 'maxlab_tftp',       path: '../cookbooks/maxlab_tftp'
+cookbook 'maxlab_nginx_repo', path: '../cookbooks/maxlab_nginx_repo'
+cookbook 'maxlab_pxe',        path: '../cookbooks/maxlab_pxe'
+cookbook 'maxlab_chrony',     path: '../cookbooks/maxlab_chrony'
+cookbook 'maxlab_postfix',    path: '../cookbooks/maxlab_postfix'
+
+# From role base_firewall_maxlab_nas
+default['config_firewall']['base_config'] = 'base_firewall.maxlab.dmz'
+
+# From role client_chrony_maxlab
+default['instance_config_chrony']['instance'] = 'chrony.maxlab'
+default['instance_config_chrony']['instance_type'] = 'client'
+
+# From role service_nginx_repo_instance_maxlab
+default['instance_config_nginx_repo']['instance'] = 'maxlab_repo'
+
+# From role service_tftp_instance_maxlab
+default['instance_config_tftp']['instance'] = 'tftp_maxlab'
+
+# From role service_pxe_instance_maxlab
+default['instance_config_pxe']['instance'] = 'pxe_server.maxlab'
+
+# From role service_postfix_instance_maxlab_postfix
+default['instance_config_postfix']['instance'] = 'maxlab_postfix'
+
+# Previously environment variables
+default['env']['maxlab']['repo_url']          = "http://repo.maxlab"
+default['env']['maxlab']['dist_url']          = "http://repo.maxlab/dist"
+default['env']['maxlab']['iso_url']           = "http://repo.maxlab/iso"
+default['env']['maxlab']['files_url']         = "http://repo.maxlab/files"
+default['env']['maxlab']['typefaces_url']     = "http://repo.maxlab/files/typefaces"
+default['env']['maxlab']['chef_url']          = "https://chef.maxlab/organizations/maxlab"
+default['env']['maxlab']['tftp_url']          = "http://repo.maxlab"
+default['env']['maxlab']['upstream_ntp']      = "2.centos.pool.ntp.org"
+default['env']['maxlab']['metric_submit_vip'] = "http://metric_submit.maxlab"
+default['env']['maxlab']['metric_query_vip']  = "http://metric_query.maxlab"
+default['env']['maxlab']['graphite_vip']      = "http://graphite.maxlab"
+default['env']['maxlab']['graphite_vip']      = "http://grafana.maxlab"
