129316bd4a2c1dab7c92474acc8a63e16aacd671
Maxwell Spangler <maxcode@maxwellspangler.com>
2022-02-11 18:04:39 -0700

    * Cookstyle cleanup: Replace double quotes with single quotes as recommended * Cookstyle cleanup: remove utf8 encoding on tests * Cookstyle cleanup: Add Redhat 8, Centos 8, remove Ubuntu from spec file * Cookstyle cleanup: Use node['platform_version'].to_i.to_s * Cookstyle cleanup: Remove long description from metadata.rb * Cookstyle cleanup: Remove non-standard comment syntax #<> * Cookstyle clenaup: Remove block comments

--- cookbooks/maxlab_tftp/recipes/deploy.rb
+++ cookbooks/maxlab_tftp/recipes/deploy.rb
@@ -2,17 +2,11 @@
 # Cookbook:: maxlab_tftp
 # Recipe:: deploy
 #
-# Copyright:: 2019, The Authors, All Rights Reserved.
-
-=begin
-#<
-Deploy a TFTP server instance to support PXE netbooting of VM nodes.
-#>
-=end
+# Copyright:: 2019, Maxwell Spangler, All Rights Reserved.
 
 config_tftp = data_bag_item('config_tftp', node['instance_config_tftp']['instance'])
 
-%W[ tftp tftp-server xinetd ].each do |pkg|
+%W( tftp tftp-server xinetd ).each do |pkg|
 
   package pkg do
     action :install
@@ -30,8 +24,8 @@ template '/etc/xinetd.d/tftp-server' do
 end
 
 # Normally created by rpm tftp-server but not re-created if the dir is removed
-directory config_tftp['pxelinux_cfg_root'] do
 # and the package is already installed
+directory config_tftp['pxelinux_cfg_root'] do
   user config_tftp['user']
   group config_tftp['group']
   mode '0755'
@@ -49,7 +43,7 @@ service_zone = node['maxlab_firewall']['default_interface_zone']
 if config_tftp['firewall']['firewalld'].key?('services')
   config_tftp['firewall']['firewalld']['services'].each do |service_string|
 
-    if not node['maxlab_firewall']['zones'][service_zone]['services'].include? service_string
+    unless node['maxlab_firewall']['zones'][service_zone]['services'].include? service_string
       node.normal['maxlab_firewall']['zones'][service_zone]['services'] << service_string
     end
 
@@ -60,7 +54,7 @@ end
 if config_tftp['firewall']['firewalld'].key?('ports')
   config_tftp['firewall']['firewalld']['ports'].each do |port_string|
 
-    if not node['maxlab_firewall']['zones'][service_zone]['ports'].include? port_string
+    unless node['maxlab_firewall']['zones'][service_zone]['ports'].include? port_string
       node.normal['maxlab_firewall']['zones'][service_zone]['ports'] << port_string
     end
 
@@ -71,7 +65,7 @@ end
 if config_tftp['firewall']['firewalld'].key?('sources')
   config_tftp['firewall']['firewalld']['sources'].each do |source_string|
 
-    if not node['maxlab_firewall']['zones'][source_zone]['sources'].include? source_string
+    unless node['maxlab_firewall']['zones'][source_zone]['sources'].include? source_string
       node.normal['maxlab_firewall']['zones'][source_zone]['sources'] << source_string
     end
 
--- cookbooks/maxlab_tftp/test/integration/default/default.rb
+++ cookbooks/maxlab_tftp/test/integration/default/default.rb
@@ -1,5 +1,3 @@
-# # encoding: utf-8
-
 # Inspec test for recipe maxlab_tftp::default
 
 # The Inspec reference, with examples and extensive documentation, can be
