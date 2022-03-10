22a2d563a1b69ca1ea57ba1d3bd2bbf24231a6e5
Maxwell Spangler <maxcode@maxwellspangler.com>
2022-03-09 19:03:36 -0700

    * Deploy 'apcupsd' for monitoring APC UPS equipment * Added recipe to runlist, cookbook dependency, config instance and long list of maxlab_apcupsd cookbook related attributes for apcupsd config file template use.

--- policyfiles/maxlab_node_nas_depot.rb
+++ policyfiles/maxlab_node_nas_depot.rb
@@ -11,6 +11,7 @@ run_list "recipe[maxlab_firewall::base_firewalld]",
          "recipe[maxlab_chrony::deploy]",
          "recipe[maxlab_nfs::server]",
          "recipe[maxlab_samba::server]",
+         "recipe[maxlab_apcupsd::deploy]",
          "recipe[maxlab_postfix::deploy]",
          "recipe[maxlab_firewall::update_firewalld]"
 
@@ -21,6 +22,7 @@ cookbook 'maxlab_smartd',   path: '../cookbooks/maxlab_smartd'
 cookbook 'maxlab_chrony',   path: '../cookbooks/maxlab_chrony'
 cookbook 'maxlab_nfs',      path: '../cookbooks/maxlab_nfs'
 cookbook 'maxlab_samba',    path: '../cookbooks/maxlab_samba'
+cookbook 'maxlab_apcupsd',  path: '../cookbooks/maxlab_apcupsd'
 cookbook 'maxlab_postfix',  path: '../cookbooks/maxlab_postfix'
 
 cookbook 'chef-vault'
@@ -41,6 +43,9 @@ default['instance_config_samba']['instance'] = 'maxlab_samba_depot'
 # From role service_postfix_instance_maxlab_postfix
 default['instance_config_postfix']['instance'] = 'maxlab_postfix'
 
+# The config_apcupsd data bag used to configure apcupsd on this node
+default['instance_config_apcupsd']['instance'] = 'maxlab_apcupsd_depot'
+
 # Previously environment variables
 default['env']['maxlab']['repo_url']          = "http://repo.maxlab"
 default['env']['maxlab']['dist_url']          = "http://repo.maxlab/dist"
@@ -54,3 +59,40 @@ default['env']['maxlab']['metric_submit_vip'] = "http://metric_submit.maxlab"
 default['env']['maxlab']['metric_query_vip']  = "http://metric_query.maxlab"
 default['env']['maxlab']['graphite_vip']      = "http://graphite.maxlab"
 default['env']['maxlab']['graphite_vip']      = "http://grafana.maxlab"
+
+# Custom settings to override default values from maxlab_apcupsd cookbook
+default['apcupsd']['config']['sysadmin']       = "maxops@maxwellspangler.com"
+
+default['apcupsd']['config']['upsname']        = "depot-backups-pro-1500"
+default['apcupsd']['config']['upscable']       = "usb"
+default['apcupsd']['config']['upstype']        = "usb"
+default['apcupsd']['config']['device']         = ""
+default['apcupsd']['config']['polltime']       = "60"
+default['apcupsd']['config']['lockfile']       = "/var/lock"
+default['apcupsd']['config']['scriptdir']      = "/etc/apcupsd"
+default['apcupsd']['config']['pwrfaildir']     = "/etc/apcupsd"
+default['apcupsd']['config']['nologindir']     = "/etc"
+default['apcupsd']['config']['onbatterydelay'] = 30
+default['apcupsd']['config']['batterylevel']   = 15
+default['apcupsd']['config']['minutes']        = 5
+default['apcupsd']['config']['timeout']        = 0
+default['apcupsd']['config']['annoy']          = 300
+default['apcupsd']['config']['annoydelay']     = 60
+default['apcupsd']['config']['nologon']        = "disable"
+default['apcupsd']['config']['killdelay']      = 0
+default['apcupsd']['config']['netserver']      = "on"
+default['apcupsd']['config']['nisip']          = "0.0.0.0"
+default['apcupsd']['config']['nisport']        = "3551"
+default['apcupsd']['config']['eventsfile']     = "/var/log/apcupsd.events"
+default['apcupsd']['config']['eventsfilemax']  = 10
+default['apcupsd']['config']['stattime']       = 0
+default['apcupsd']['config']['statfile']       = "/var/log/apcupsd.status"
+default['apcupsd']['config']['logstats']       = "off"
+default['apcupsd']['config']['datatime']       = 0
+default['apcupsd']['config']['facility']       = "daemon"
+
+default['apcupsd']['config']['enable_logger']  = true
+default['apcupsd']['config']['log_month']      = "*"
+default['apcupsd']['config']['log_day']        = "*"
+default['apcupsd']['config']['log_hour']       = "12"
+default['apcupsd']['config']['log_minute']     = "0"
