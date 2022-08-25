4f798baf7a420772254b2db1bfa8b35d584eb0cd
Maxwell Spangler <maxcode@maxwellspangler.com>
2022-08-24 22:14:54 -0600

    Supply named.conf template with a list of ipv4 and ipv6 addresses to listen on so a single DHCP server can support multiple networks/subnets. Configure forward DNS files based on subnet domain names rather than overall network ids. This allows a single DHCP server to support multiple networks/subnets. Cookbstyle cleanup overall with lots of extra line spacing added.

--- cookbooks/maxlab_bind/recipes/deploy.rb
+++ cookbooks/maxlab_bind/recipes/deploy.rb
@@ -20,18 +20,26 @@ node['bind']['packages'][os_dist][os_major].each do |pkg, ver|
 
   if ver == 'any'
     package pkg do
+
       action :install
+
     end
   else
     package pkg do
+
       version ver
       action :install
+
     end
   end
+
 end
 
 # Start with empty hash of zones, add them each time we drop fwd/rev config files
 config_zones = {}
+
+config_listen_on_ipv4_list = []
+config_listen_on_ipv6_list = []
 
 # Load the bind config attributes. This used to be from a data bag.
 config_bind = node['bind']
@@ -41,31 +49,73 @@ config_bind = node['bind']
 # resolution files.  This allows easy config for a home lab + limited DMZ DNS
 
 config_bind['config_network_ids'].each do |cfg_net_id|
+
   # Find network information to support deploying each bind instance
   config_network = data_bag_item('config_network', cfg_net_id)
 
+  log "log-config-network-#{cfg_net_id}" do
+
+    message "debug-log-config-cfg_net_id: #{cfg_net_id}"
+    level :debug
+
+  end
+
   # Iterate over each subnet defined in the network data bag configuration
-  config_network['subnet'].each do |_network_name, subnet|
+  config_network['subnet'].each do |network_name, subnet|
+
+    log "log-cn-subnet-#{network_name}" do
+
+      message "debug-log-cn-subnet network-name: #{network_name}"
+      message "debug-log-cn-subnet subnet #{subnet['domain_name']}"
+      level :debug
+
+    end
+
+    # Append this subnet's listen on address here
+    # A single DHCP config may listen on multiple network subnets
+    unless subnet['dns']['listen_on_address_ipv4'].empty?
+      config_listen_on_ipv4_list.append(subnet['dns']['listen_on_address_ipv4'])
+    end
+
+    # Append this subnet's listen on address here
+    # A single DHCP config may listen on multiple network subnets
+    unless subnet['dns']['listen_on_address_ipv6'].empty?
+      config_listen_on_ipv6_list.append(subnet['dns']['listen_on_address_ipv6'])
+    end
 
     # Add each forward and reverse zone config to config_zones.
     # This will be used to configure /etc/named.conf later
     subnet['dns']['zones'].each do |zone_name, zone_cfg|
+
       config_zones[zone_name] = zone_cfg
 
       # Remove '.in-addr.arpa' from filenames, leaving 'db.9.168.192' (example)
       config_zones[zone_name]['fname'] = zone_name.gsub('.in-addr.arpa', '')
+
     end
 
     # Construct a unique list of groups by scanning all nodes here
     groups = []
     subnet['nodes'].each do |_ip, node_cfg|
+
       unless groups.include? node_cfg['group']
         groups << node_cfg['group']
       end
+
     end
 
     # Config file name, ex: db.maxlab
-    fname_forward = "/var/named/db.#{cfg_net_id}"
+    # Was ok with cfg_net_id when only processing id:maxlab and single subnet
+    # But now we need to configure each subnet via its domain_name
+    # fname_forward = "/var/named/db.#{cfg_net_id}"
+    fname_forward = "/var/named/db.#{subnet['domain_name']}"
+
+    log "log-cn-subnet-#{network_name}" do
+
+      message "debug-log-fname_forward #{fname_forward}"
+      level :debug
+
+    end
 
     # Convert network prefix '192.168.9' to '9.168.192'
     reverse_network = subnet['network_prefix'].split('.')[2] + '.' + subnet['network_prefix'].split('.')[1] + '.' + subnet['network_prefix'].split('.')[0]
@@ -75,6 +125,7 @@ config_bind['config_network_ids'].each do |cfg_net_id|
 
     # Deploy the forward resolution DNS file
     template fname_forward do
+
       source 'db.forward.erb'
       variables(
         {
@@ -84,14 +135,16 @@ config_bind['config_network_ids'].each do |cfg_net_id|
           bind_cfg: subnet['dns'],
           aliases: subnet['aliases'],
           nodes: subnet['nodes'],
-          groups: groups,
+          groups: groups
         }
       )
       notifies :reload, 'service[named]', :delayed
+
     end
 
     # Deploy the reverse resolution DNS file
     template fname_reverse do
+
       source 'db.reverse.erb'
       variables(
         {
@@ -100,43 +153,60 @@ config_bind['config_network_ids'].each do |cfg_net_id|
           bind_serial_string: bind_serial_string,
           bind_cfg: subnet['dns'],
           nodes: subnet['nodes'],
-          groups: groups,
+          groups: groups
         }
       )
       notifies :reload, 'service[named]', :delayed
+
     end
+
   end
+
 end
 
 # Auto configure listening IP address for ipv4 or use a specified value
-cfg_listen_ipv4 = case config_bind['config_listen_on_address_ipv4']
-                  when 'ipaddress'
-                    node['ipaddress']
-                  else
-                    config_bind['listen_on_address_ipv4']
-                  end
+# cfg_listen_ipv4 = case config_bind['config_listen_on_address_ipv4']
+#                   when 'ipaddress'
+#                     node['ipaddress']
+#                   else
+#                     config_bind['listen_on_address_ipv4']
+#                   end
+#
+# # Auto configure listening IP address for ipv6 or use a specified value
+# cfg_listen_ipv6 = case config_bind['config_listen_on_address_ipv6']
+#                   when 'ipaddress'
+#                     node['ipaddress']
+#                   else
+#                     config_bind['listen_on_address_ipv6']
+#                   end
 
-# Auto configure listening IP address for ipv6 or use a specified value
-cfg_listen_ipv6 = case config_bind['config_listen_on_address_ipv6']
-                  when 'ipaddress'
-                    node['ipaddress']
-                  else
-                    config_bind['listen_on_address_ipv6']
-                  end
+log "log-namedconf" do
+
+  message "debug-log-cn-subnet config_bind: #{config_bind}"
+  message "debug-log-cn-subnet config_zones #{config_zones}"
+  message "debug-log-cn-subnet config_zones #{config_listen_on_ipv4_list}"
+  message "debug-log-cn-subnet config_zones #{config_listen_on_ipv6_list}"
+  level :debug
+
+end
 
 # Deploy master bind config file
 template '/etc/named.conf' do
+
   source 'named.conf.erb'
   variables(
     {
       config_bind: config_bind,
       config_zones: config_zones,
-      cfg_listen_ipv4: cfg_listen_ipv4,
-      cfg_listen_ipv6: cfg_listen_ipv6,
+      # cfg_listen_ipv4: cfg_listen_ipv4,
+      # cfg_listen_ipv6: cfg_listen_ipv6,
+      config_listen_on_ipv4_list: config_listen_on_ipv4_list,
+      config_listen_on_ipv6_list: config_listen_on_ipv6_list
     }
   )
 
   notifies :reload, 'service[named]', :delayed
+
 end
 
 # Warning - firewalld specific, won't work on Red hat < 7, Debian, etc
@@ -153,6 +223,7 @@ if config_bind['firewall']['firewalld'].key?('services')
     unless node['maxlab_firewall']['zones'][service_zone]['services'].include? service_string
       node.normal['maxlab_firewall']['zones'][service_zone]['services'] << service_string
     end
+
   end
 end
 
@@ -163,15 +234,18 @@ if config_bind['firewall']['firewalld'].key?('ports')
     unless node['maxlab_firewall']['zones'][service_zone]['ports'].include? port_string
       node.normal['maxlab_firewall']['zones'][service_zone]['ports'] << port_string
     end
+
   end
 end
 
 # Append individual sources definitions
 if config_bind['firewall']['firewalld'].key?('sources')
   config_bind['firewall']['firewalld']['sources'].each do |source_string|
+
     unless node['maxlab_firewall']['zones'][source_zone]['sources'].include? source_string
       node.normal['maxlab_firewall']['zones'][source_zone]['sources'] << source_string
     end
+
   end
 end
 
@@ -179,8 +253,10 @@ end
 
 # Ensure the service starts on boot and is reloaded upon config file updates
 service 'named' do
+
   subscribes :reload, 'template[/etc/named.conf]', :delayed
   action [:enable, :start]
+
 end
 
 # Now that we've deployed a DNS server, tag this node as a DNS server
